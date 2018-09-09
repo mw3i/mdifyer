07052f1986279bfbdeb602aa0ab0354a36ba86a9
mw <mwetzel7r@gmail.com>
2018-09-09 09:19:36 -0400

    added "dmdifyer" function

--- mdifyer.py
+++ mdifyer.py
@@ -2,16 +2,21 @@ import sys
 import os
 
 '''md
-This script can mdfy itself
+This script can mdify itself
 
 '''
+language_info = {
+    'python': {
+        'block_comment': ["'''", "'''"],
+        'extension': '.py'
+    },
+    'javascript': {
+        'block_comment': ["//","//"],
+        'extension': '.js'
+    }
+}
 def mdifyer(input_script_path, output_script_path=None, language='python'):
-    block_comment = {
-        'python':"'''",
-        'javascript': '//' #<-- i think
-        # more languages coming whenever
-    }[language]
-
+    block_comment = language_info[language]['block_comment']
 
     if output_script_path == None:
         output_script_path = os.path.splitext(input_script)[0]+'.md'
@@ -19,16 +24,16 @@ def mdifyer(input_script_path, output_script_path=None, language='python'):
     ## open text file containing script
     new_text = []
     with open(input_script_path, 'r') as file:
-
         ## check for '''
         text = file.read()
         while text != '':
             try:
-                new_text.append(text[:text.index("'''md")])
-                text = text[text.index(block_comment+"md")+3:]
-                new_text.append(text[:text.index(block_comment)])
-                text = text[text.index(block_comment)+3:]
-            except:
+                new_text.append(text[:text.index(block_comment[0] + "md")])
+                text = text[text.index(block_comment[0] + "md") + len(block_comment[0]):]
+                new_text.append(text[:text.index(block_comment[1])])
+                text = text[text.index(block_comment[1])+len(block_comment[1]):]
+            except Exception as e:
+                # raise(e)
                 new_text.append(text)
                 text = ''
 
@@ -46,12 +51,52 @@ def mdifyer(input_script_path, output_script_path=None, language='python'):
 
             # if it doesn't, code block it
             else:
-                new_md_file.write('```python\n'+block+'\n```\n')
+                new_md_file.write('```'+language+'\n'+block+'\n```\n')
+
+
+
+def dmdifyer(input_script_path, output_script_path=None, language='python'):
+    block_comment = language_info[language]['block_comment']
+    extension = language_info[language]['extension']
+
+    if output_script_path == None:
+        output_script_path = os.path.splitext(input_script)[0]+extension
+
+    ## open text file containing script
+    new_text = []
+    with open(input_script_path, 'r') as file:
+
+        ## check for '''
+        text = file.read()
+        while text.replace('\n','').replace('\t','').replace(' ','') != '':
+            try:
+                new_text.append(text[:text.index("```"+language)])
+                text = text[text.index("```"+language)+3:]
+                new_text.append(text[:text.index("```")])
+                text = text[text.index("```")+3:]
+            except:
+                new_text.append(text)
+                text = ''
+
+    ## write results to new script
+    with open(output_script_path, 'w') as new_script:
+
+        ## iterate though text blocks
+        if new_text[0] == '': new_text = new_text[1:]
+        for block in new_text:
+
+        ## check if block is an md code block
+            # if it is, write it to text normally
+            if block[:len(language)] == language:
+                new_script.write(block[len(language):])
+
+            # if it isn't, block comment it
+            else:
+                new_script.write(block_comment[0] + 'md\n' + block + '\n' + block_comment[1] + '\n')
+
+
 
 if __name__ == '__main__':
-    input_script_path = 'example_script.py'
-    mdifyer(input_script_path, output_script_path='README.md')
+    mdifyer('example_mdify_script.py', output_script_path='README.md', language='python')
 
-
-
-
+    dmdifyer('README.md', output_script_path='example_dmdify_script.py', language='python')
